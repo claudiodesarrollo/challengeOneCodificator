dfe7e96a5739461c695ad6ef52bd8473cfd40959
Quipildor Claudio <claudioq00@outlook.com>
2022-05-12 02:28:37 -0300

    encriptar y desencriptar funcionando

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-var input = document.querySelector("textarea");
+
+const input = document.querySelector(".area-entrada");
+const mensageSent = document.querySelector(".text-result");
+
+function miKeys() {
+    var matrizKeys = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    return matrizKeys;
+}
+
+function encrypt(mensage) {
+    for (let i = 0; i < miKeys().length; i++) {
+        if (mensage.includes(miKeys()[i][0])) {
+            mensage = mensage.replaceAll(miKeys()[i][0], miKeys()[i][1]);
+        }
+    }
+
+    return mensage;
+}
+
+function unlock(mensage){
+    for (let i = 0; i < miKeys().length; i++) {
+        if (mensage.includes(miKeys()[i][1])) {
+            mensage = mensage.replaceAll(miKeys()[i][1], miKeys()[i][0]);
+        }
+    }
+
+    return mensage;
+
+}
 
 
-function validar(){
+function btnEncrypt() {
+    mensageSent.value = encrypt(input.value);
 
+}
+function btnUnlock() {
+    mensageSent.value = unlock(input.value);
 }
