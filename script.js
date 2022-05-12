ba2ec1dcad05d530995a67e22be72739bff08afd
Quipildor Claudio <claudioq00@outlook.com>
2022-05-12 19:46:47 -0300

    Desifrador terminado - falta breakpoint

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ function encrypt(mensage) {
             mensage = mensage.replaceAll(miKeys()[i][0], miKeys()[i][1]);
         }
     }
-
+    cover();
     return mensage;
 }
 
-function unlock(mensage){
+function unlock(mensage) {
     for (let i = 0; i < miKeys().length; i++) {
         if (mensage.includes(miKeys()[i][1])) {
             mensage = mensage.replaceAll(miKeys()[i][1], miKeys()[i][0]);
@@ -31,8 +31,22 @@ function unlock(mensage){
 
 function btnEncrypt() {
     mensageSent.value = encrypt(input.value);
-
+    cover();
 }
 function btnUnlock() {
     mensageSent.value = unlock(input.value);
+}
+
+function cover() {
+    document.querySelector(".muneco").classList.add("hidden");
+    document.querySelector(".text1").classList.add("hidden");
+    document.querySelector(".text2").classList.add("hidden");
+    document.querySelector(".text-result").classList.add("primero");
+    document.querySelector(".copiar").classList.remove("hidden");
+}
+function copiarTexto() {
+    const text = document.querySelector(".text-result").value;
+    navigator.clipboard.writeText(text);
+
+    return alert("El texto :" + " " + text + " " + "fue copiado al pora papeles");
 }
